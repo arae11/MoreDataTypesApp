903da08759a87a4fd237807e6f3abc058ab24a52
Alex Rae <arae@spartaglobal.com>
2021-07-30 13:03:14 +0100

    String Exercise

---
+++ MoreDataTypes/MoreDataTypesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MoreDataTypesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = " C# list fundementals ";
+            Console.WriteLine(StringExercise(myString));
+        }
+        public static string StringExercise(string myString)
+        {
+            var trimString = myString.Trim();
+            var ucString = trimString.ToUpper();
+            var replaceString = ucString.Replace('L', '*').Replace('T', '*');
+            var nPos = replaceString.IndexOf('N');
+            var finalString = replaceString.Remove(nPos + 1);
+            return finalString;
+        }
+    }
+}
