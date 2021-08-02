86e758b75c4f69e205456b89a33fecf2fff02cab
Alex Rae <arae@spartaglobal.com>
2021-08-02 11:15:40 +0100

    Finished Everything

--- MoreDataTypes/MoreDataTypesApp/Program.cs
+++ MoreDataTypes/MoreDataTypesApp/Program.cs
@@ -7,6 +7,7 @@ namespace MoreDataTypesApp
     {
         static void Main(string[] args)
         {
+
             //Console.WriteLine("How many apples?");
             //string input = Console.ReadLine();
             //int numApples = Int32.Parse(input);
@@ -22,97 +23,157 @@ namespace MoreDataTypesApp
             //var fString2 = $"Log to the base {num2} of {num1} is {Math.Log(num2, num1):0####}";
             //var fString3 = $"That will be {num2 / 3.0:C} please!";
 
-            int[] myIntArray = { 12, 23, 34, 45, 56 };
+            //int[] myIntArray = { 12, 23, 34, 45, 56 };
 
-            Array.Reverse<int>(myIntArray);
+            //Array.Reverse<int>(myIntArray);
 
-            foreach (var element in myIntArray)
-            {
-                Console.WriteLine(element);
-            }
-            MultiDArrays();
-            JaggedArrays();
+            //foreach (var element in myIntArray)
+            //{
+            //    Console.WriteLine(element);
+            //}
+            //MultiDArrays();
+            //JaggedArrays();
 
             //grid[0, 1] = 6; - multi d array
             //grid[0][1] = 6; - jagged array
 
+            //DateTimeMethods();
+            Enums();
+            Enums2();
         }
-        public static void MultiDArrays()
+
+        public enum Suit
         {
-            int[,] grid = new int[2, 4];
-            grid[0, 1] = 6;
-            grid[1, 0] = 8;
-            grid[1, 3] = 10;
-
-
-            char[,] gridTwo = { { 'a', 'b' },
-                                { 'c', 'd' },
-                                { 'e', 'f' },
-                                { 'g', 'h' } };
-            Console.WriteLine("All of grid two");
-
-            foreach(var element in gridTwo)
+            HEARTS, CLUBS, DIAMONDS, SPADES
+        }
+        public static void Enums()
+        {
+            Suit theSuit = Suit.HEARTS;
+            if (theSuit == Suit.SPADES)
             {
-                Console.WriteLine($"{element}");
+                Console.WriteLine($"The Suit is {Suit.SPADES}");
             }
-
-            for (int i = gridTwo.GetLowerBound(0); i <= gridTwo.GetUpperBound(0); i++)
+            else
             {
-                for (int j = gridTwo.GetLowerBound(1); j <= gridTwo.GetUpperBound(1); j++)
-                {
-                    Console.WriteLine($"({i},{j}) {gridTwo[i, j]}  ");
-                }
-                Console.WriteLine();
+                Console.WriteLine($"The Suit is {theSuit} not {Suit.SPADES}");
+            }
+            var mediumValue = (int)Size.MEDIUM;
+            var mySize = (Size)50;
+
+            var anotherSuit = Enum.Parse(typeof(Suit), "CLUBS");
+
+        }
+        public static void Enums2()
+        {
+            Suit theSuit = Suit.HEARTS;
+            switch (theSuit)
+            {
+                case Suit.HEARTS:
+                    Console.WriteLine("Thank You");
+                    break;
+                default:
+                    Console.WriteLine("Goodbye");
+                    break;
             }
         }
+
+        public enum Size
+        {
+            SMALL = 10, MEDIUM = 25, LARGE = 50, XLARGE = 75
+        }
+
+        public static void
+
+
+
+
+        public static void DateTimeMethods()
+        {
+            var now = DateTime.Now;
+            Console.WriteLine($"The time is: {now}");
+            Console.WriteLine($"In ticks this is: {now.Ticks}");
+            var tomorrow = now.AddDays(1);
+            Console.WriteLine($"Tomorrow it will be: {tomorrow} which is {tomorrow.Ticks}");
+            now.
+        }
+
+
+        //public static void MultiDArrays()
+        //{
+        //    int[,] grid = new int[2, 4];
+        //    grid[0, 1] = 6;
+        //    grid[1, 0] = 8;
+        //    grid[1, 3] = 10;
+
+
+        //    char[,] gridTwo = { { 'a', 'b' },
+        //                        { 'c', 'd' },
+        //                        { 'e', 'f' },
+        //                        { 'g', 'h' } };
+        //    Console.WriteLine("All of grid two");
+
+        //    foreach(var element in gridTwo)
+        //    {
+        //        Console.WriteLine($"{element}");
+        //    }
+
+        //    for (int i = gridTwo.GetLowerBound(0); i <= gridTwo.GetUpperBound(0); i++)
+        //    {
+        //        for (int j = gridTwo.GetLowerBound(1); j <= gridTwo.GetUpperBound(1); j++)
+        //        {
+        //            Console.WriteLine($"({i},{j}) {gridTwo[i, j]}  ");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
         
-        public static void JaggedArrays() // jagged array means not an even number of rows/cols - not square
-        {
-            int[][] intJArray = new int[2][];
-            intJArray[0] = new int[4];
-            intJArray[1] = new int[2];
+        //public static void JaggedArrays() // jagged array means not an even number of rows/cols - not square
+        //{
+        //    int[][] intJArray = new int[2][];
+        //    intJArray[0] = new int[4];
+        //    intJArray[1] = new int[2];
 
-            intJArray[0][2] = 3;
-            intJArray[1][0] = 5;
+        //    intJArray[0][2] = 3;
+        //    intJArray[1][0] = 5;
 
-            string[][] animalJArray = new string[][]
-            {
-                new string[] { "llama", "puma", "horse", "kitten" },
-                new string[] { "haddock", "tuna"}
-            };
-            Console.WriteLine("Jagged array animals");
-            foreach(var row in animalJArray)
-            {
-                foreach(var entry in row)
-                {
-                    Console.WriteLine($"{entry}");
-                }
-            }
+        //    string[][] animalJArray = new string[][]
+        //    {
+        //        new string[] { "llama", "puma", "horse", "kitten" },
+        //        new string[] { "haddock", "tuna"}
+        //    };
+        //    Console.WriteLine("Jagged array animals");
+        //    foreach(var row in animalJArray)
+        //    {
+        //        foreach(var entry in row)
+        //        {
+        //            Console.WriteLine($"{entry}");
+        //        }
+        //    }
             
-        }
+        //}
 
-        public static void StringInterpolation(string str)
-        {
-            Console.WriteLine("My name is : " + str + " using +"); // concatennation 
-            Console.WriteLine($"My name is : {str} using interpolation"); // interpolation
-        }
-        public static string StringBuilderExercise(string myString)
-        {
-            var trimmedUpperString = myString.Trim().ToUpper();
-            var nPos = trimmedUpperString.IndexOf('N');
-            StringBuilder sb = new StringBuilder(trimmedUpperString);
-            sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, (sb.Length - nPos - 1));
-            return sb.ToString();
-        }
+        //public static void StringInterpolation(string str)
+        //{
+        //    Console.WriteLine("My name is : " + str + " using +"); // concatennation 
+        //    Console.WriteLine($"My name is : {str} using interpolation"); // interpolation
+        //}
+        //public static string StringBuilderExercise(string myString)
+        //{
+        //    var trimmedUpperString = myString.Trim().ToUpper();
+        //    var nPos = trimmedUpperString.IndexOf('N');
+        //    StringBuilder sb = new StringBuilder(trimmedUpperString);
+        //    sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, (sb.Length - nPos - 1));
+        //    return sb.ToString();
+        //}
 
-        public static string StringExercise(string myString)
-        {
-            var trimString = myString.Trim();
-            var ucString = trimString.ToUpper();
-            var replaceString = ucString.Replace('L', '*').Replace('T', '*');
-            var nPos = replaceString.IndexOf('N');
-            var finalString = replaceString.Remove(nPos + 1);
-            return finalString;
+        //public static string StringExercise(string myString)
+        //{
+        //    var trimString = myString.Trim();
+        //    var ucString = trimString.ToUpper();
+        //    var replaceString = ucString.Replace('L', '*').Replace('T', '*');
+        //    var nPos = replaceString.IndexOf('N');
+        //    var finalString = replaceString.Remove(nPos + 1);
+        //    return finalString;
 
             //var intermediateString = myString.Trim().ToUpper().Replace('L', '*').Replace('T', '*')'
             //var nPost = intermediateString.IndexOf('N');
@@ -120,4 +181,4 @@ namespace MoreDataTypesApp
             //return finalString;
         }
     }
-}
+
--- MoreTypes_Lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
+++ MoreTypes_Lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
@@ -11,24 +11,47 @@ namespace MoreTypes_Lib
         // returns a person's age at a given date, given their birth date.
         public static int AgeAt(DateTime birthDate, DateTime date)
         {
-            return -1;
+            if (birthDate > date)
+            {
+                throw new ArgumentException("Error - birthDate is in the future");
+            }
+            return (int)((date - birthDate).TotalDays / 365.25);
         }
+
         // returns a date formatted in the manner specified by the unit test
         public static string FormatDate(DateTime date)
         {
-            return string.Empty;
+            var result = ($"{date:yy/dd/MMM}");
+            return result;
         }
 
         // returns the name of the month corresponding to a given date
         public static string GetMonthString(DateTime date)
         {
-            return string.Empty;
+            var result = ($"{date:MMMM}");
+            return result;
         }
 
         // see unit tests for requirements
         public static string Fortune(Suit suit)
         {
-            return string.Empty;
+            string result = string.Empty;
+            switch (suit)
+            {
+                case Suit.CLUBS:
+                    result = ("And the seventh rule is if this is your first night at fight club, you have to fight.");
+                    break;
+                case Suit.DIAMONDS:
+                    result = ("Diamonds are a girls best friend");
+                    break;
+                case Suit.HEARTS:
+                    result = ("You've broken my heart");
+                    break;
+                case Suit.SPADES:
+                    result = ("Bucket and spade");
+                    break;
+            }
+            return result;
         }
     }
 }
